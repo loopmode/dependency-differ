460ad91e2595d4096a8844124730e8c87491a258
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-12-18 12:08:04 +0100

    add build for github pages via /docs folder

---
+++ docs/precache-manifest.b6d70b942b51a47aa7c583555c034a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab53193643566a6e124e7b11d23c211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "194baa4d17f27a8b0874",
+    "url": "/static/css/main.918d480d.chunk.css"
+  },
+  {
+    "revision": "4ece599368f42787234f",
+    "url": "/static/js/2.a1898755.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a1898755.chunk.js.LICENSE"
+  },
+  {
+    "revision": "194baa4d17f27a8b0874",
+    "url": "/static/js/main.6b1e95f1.chunk.js"
+  },
+  {
+    "revision": "7f47b0c37ed65d037aa5",
+    "url": "/static/js/runtime-main.de2f2e40.js"
+  }
+]);
